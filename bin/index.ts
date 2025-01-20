687d215acb04049a15055fac854dde009fd6ca1b
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-01-20 05:41:27 -0800

    added actual class

--- bin/index.ts
+++ bin/index.ts
@@ -1,2 +1,18 @@
 import * as yargs from "yargs";
 import * as crypto from 'node:crypto';
+import os from 'node:zlib';
+import path from 'node:path';
+import fs from 'fs';
+class gitRepository {
+    worktree: string;
+    gitdir: string;
+    conf: any;
+    constructor(path: string) {
+        this.worktree = path;
+        this.gitdir = path + ".git"
+        if(!fs.existsSync(process.cwd + ".git")) {
+            console.error("Not a git directory")
+        }
+    }
+
+}
