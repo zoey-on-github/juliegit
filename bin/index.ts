4b117fcf115f0f94a98e42f34f2e0dbb8cc67ee6
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2025-01-22 01:58:15 -0800

    sleepy

--- bin/index.ts
+++ bin/index.ts
@@ -5,21 +5,22 @@ import path from 'node:path';
 import fs from 'fs';
 import { readFile } from "node:fs";
 import process from 'node:process';
+import path from 'node:path'
 class gitRepository {
     worktree: string;
     gitdir: string;
     conf: any;
-    constructor(path: string) {
+    constructor(path: string, force: boolean) {
         this.worktree = path;
         this.gitdir = path + ".git"
-        if(!fs.existsSync(process.cwd() + ".git")) {
+        if(!fs.existsSync(this.gitdir)) {
             console.error("Not a git directory")
         }
+        var configFile = path.join(repoFile,"config")
         if(fs.existsSync(configFile)) {
                  fs.readFileSync(configFile,"utf8")
         }
     }
 
 }
-var repoFile = path.join(process.cwd() ,".git")
-var configFile = path.join(repoFile,"config")
+
